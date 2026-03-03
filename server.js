569adaf07ce53638ed4e717a574a3c3059f6fbef
KAIO JUNCKES DILLI <kaio_dilli@estudante.sesisenai.org.br>
2026-03-03 12:37:13 -0300

    mensagem do commit

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 import Fastify from 'fastify'
+import { Pool } from 'pg'
 
-const servidor = Fatify();
+const sql = new Pool({
+    user: "postgres", 
+    password: "senai",
+    host: "localhost",
+    port: 5432,
+    databese: "receitas"
+})
+
+const servidor = Fastify();
 
 servidor.get('/usuarios', () => {
     return 'funcionando!'
+})
+
+servidor.post('/usuarios', async ( request, reply) => {
+   const body = request.body;
+
+const resultado = await sql.query('select * from usuarios')
+
+   return resultado.rows
+
 })
 
 servidor.listen({
