ca21d91cb10edcc54021ca3f06a62d7f9ab352ce
KAIO JUNCKES DILLI <kaio_dilli@estudante.sesisenai.org.br>
2026-03-05 12:24:20 -0300

    mensagem do commit

--- server.js
+++ server.js
@@ -18,9 +18,32 @@ servidor.get('/usuarios', async () => {
 
 servidor.post('/usuarios', async ( request, reply) => {
    const body = request.body;
-   const resultado = await sql.query('INSERT INTO usuario (nome,senha) VALUES ($1, $2)', [body.nome, body.senha])
+
+   if(!body || !body.nome || !body.senha){
+       return reply.status(400).send({message:"nome e senha obrigatórios"})
+}
+   
+
+    const resultado = await sql.query('INSERT INTO usuario (nome,senha) VALUES ($1, $2)', [body.nome, body.senha])
    return 'usuario cadastrado!'
 })
+
+
+servidor.put('/usuarios/:id', async (request, reply) =>{
+    const body = request.body;
+    const id = request.params.id;
+    const resultado = await sql.query('UPDATE usuario SET nome = $1, senha = $2 WHERE id = $3', [body.nome, body.senha, id])
+    return 'Usuario alterado!'
+})
+
+servidor.delete('/usuarios/:id', async (request, reply) =>{
+    const id = request.params.id
+    const resultado = await sql.query('DELETE FROM usuario where id = $1',[id])
+    reply.status (200).send({message:'Usuario deletado!'})
+    
+})
+
+
 
 
 servidor.listen({
